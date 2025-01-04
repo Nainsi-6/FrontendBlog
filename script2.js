c76886699dc5c82ea9a54e32e069891977ac0749
Nainsi Gupta <143492732+Nainsi-6@users.noreply.github.com>
2025-01-04 23:55:05 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -28,7 +28,7 @@ loginForm.addEventListener('submit', async (event) => {
   const password = loginForm.password.value;
 
   try {
-    const response = await fetch('http://localhost:5005/login', {
+    const response = await fetch('https://backendblog-5.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
